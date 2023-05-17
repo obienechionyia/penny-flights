7b8df818616593bb0e99f4bc1eb58eaa28d7d632
Obi Enechionyia <obienechionyia@gmail.com>
2023-05-17 15:45:37 +0200

    Styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,22 @@ import React from "react";
 import DestinationList from "../components/DestinationList";
 import SearchForm from "../components/SearchForm";
 import travel from "../assets/images/travel.svg";
+import { useGlobalContext } from "../context";
 
 const Home = () => {
+  const { destinations } = useGlobalContext();
+  console.log(destinations.length);
   return (
     <main className="main-container">
       <SearchForm />
       <DestinationList />
-      <img src={travel} alt="vacation" className="vacation-img" />
+      <img
+        src={travel}
+        alt="vacation"
+        className={
+          Number(destinations.length) > 0 ? "vacation-img-hide" : "vacation-img"
+        }
+      />
     </main>
   );
 };
